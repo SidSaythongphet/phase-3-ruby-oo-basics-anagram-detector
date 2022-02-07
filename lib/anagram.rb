6ac3322c2f9d89119e5707e517a5597f1e790470
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-02-07 15:12:48 -0500

    commit tree dcd70241fea6299cba9f4aaea674dab3d8b25d58

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        result = []
+        sorted_word = @word.chars.sort.join
+        sorted_array = arr.map { |word| word.chars.sort.join }
+        target_index = sorted_array.map.with_index { |word, index| index if word == sorted_word}.compact
+        target_index.map { |index| result << arr[index] }
+        result
+    end
+end
+
